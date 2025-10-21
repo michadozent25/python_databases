6966185caca6aa5f36381e212a35505e2a083275
micha <michajava8@gmail.com>
2025-10-21 10:41:43 +0200

    crud

--- 05_alchemy_user/crud.py
+++ 05_alchemy_user/crud.py
@@ -1,10 +1,12 @@
 from models import User
 from sqlalchemy.orm import Session
+from sqlalchemy import text, select
 
 
 def create_user(session:Session, user:User)->User:
     session.add(user)
     session.commit()
+    session.refresh(user)
     return user
 
 def get_all_users(session:Session)-> list[User]:
@@ -25,3 +27,24 @@ def get_user_by_name(session:Session,name:str)->list[User]:
 
 def user_exists(session:Session,email:str)->bool:
     return session.query(User).filter(User.email==email).count()==1
+
+def update_user(session:Session,user:User) -> User|None:
+    ''' User(id=1,email="max@web.de")'''
+    db_user = session.get(User,user.id)
+    if not db_user:
+        return None
+    # auch generisch möglich
+    if user.name is not None:  
+        db_user.name = user.name
+    if user.email is not None:
+        db_user.email = user.email
+    session.commit()
+    session.refresh(db_user)
+    return db_user
+def get_users_by_name(session:Session, name:str)->list[User]:
+    stmt = text("select * from user where name ilike :name")
+    return session.execute(stmt, {"name":f"%{name}%"})
+
+def get_users_by_name(session:Session, name:str)->list[User]:
+    stmt = select(User).where(User.name.ilike(f"%{name}%"))
+    return session.execute(stmt).scalars().all()
--- 05_alchemy_user/database.py
+++ 05_alchemy_user/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-
+#postgresql+psycopg2://postgres:@localhost:5432/db_python05
 engine = create_engine("mysql+pymysql://root:@localhost:3306/db_python04",echo=True)# mysql+pymysql://USERNAME:PASSWORD@HOST:PORT/DATENBANK
-Session = sessionmaker(bind=engine) # hier entsteht eine Klasse
+Session = sessionmaker(bind=engine) # hier entsteht eine Klasse -type(...)
 session = Session() # Session-Objekt wird erzeugt
 
 Base = declarative_base()
