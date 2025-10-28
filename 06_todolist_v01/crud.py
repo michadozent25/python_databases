5bd8c6182f8219c614ef98c9dd2eebb768fd696d
micha <michajava8@gmail.com>
2025-10-28 11:05:16 +0100

    Todolist v1-get_todos

--- 06_todolist_v01/crud.py
+++ 06_todolist_v01/crud.py
@@ -10,17 +10,18 @@ class TodoRepository:
     def create_todo(self,todo:Todo)-> Todo:
         self.session.add(todo)
         self.session.commit()
+        self.session.refresh(todo)
         return todo
     # def find_all_todos(self)->list[Todo]:
     #     pass
     def new_todo_by_user(self,user_id:int,todo:Todo)->Todo:
         user = self.session.get(User,user_id)
         user.todos.append(todo)
-        
         self.session.commit()
         self.session.refresh(todo)
         return todo
-
+    def get_todos_by_user(self,user_id:int)->list[Todo]:
+        return self.session.query(Todo).filter(Todo.user_id== user_id).all()
     ################ später #########################
     #deleteById, update/updateState, findByTask,  findByState
 
@@ -30,6 +31,7 @@ class UserRepository:
     def create_user(self,user:User)->User:
         self.session.add(user)
         self.session.commit()
+        self.session.refresh(user)
         return user
     # def find_all_users(self)->list[User]:
     #     pass
